7d176df6cfa3c5fb48c93ea323ecc240684bda1a
bsekeroglu70 <bsekeroglu70@stuy.edu>
2025-10-20 18:50:17 -0400

    PlaylistManager.kt

---
+++ app/src/main/java/edu/stuyvesant/abcdefghijklmnopqrstuvwxyz/PlaylistManager.kt
@@ -0,0 +1,21 @@
+package edu.stuyvesant.abcdefghijklmnopqrstuvwxyz
+
+object PlaylistManager {
+    private val playlists: HashMap<String, Playlist> = HashMap()
+
+    fun addNewPlaylist(name: String) {
+        throw NotImplementedError()
+    }
+
+    fun removePlaylist(name: String) {
+        throw NotImplementedError()
+    }
+
+    fun renamePlaylist(name_old: String, name_new: String) {
+        throw NotImplementedError()
+    }
+
+    fun getPlaylist(name: String) {
+        throw NotImplementedError()
+    }
+}
